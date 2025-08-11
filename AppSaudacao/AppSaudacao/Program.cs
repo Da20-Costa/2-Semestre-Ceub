15a9edb4dcc28ee2c6edf5ddc88e188530f27978
Davi Costa <da20.sousacosta@gmail.com>
2025-08-11 19:33:53 -0300

    teste

---
+++ AppSaudacao/AppSaudacao/Program.cs
@@ -0,0 +1,5 @@
+﻿string nome, saudacao;
+Console.WriteLine("Informe o seu nome: ");
+nome = Console.ReadLine();
+saudacao = "Seja bem-vindo " + nome + ", ao curso de P.O.O";
+Console.WriteLine(saudacao);
