5af442b9249099890dfb6cd654d0a06e5ce2bc03
Davi Costa <da20.sousacosta@gmail.com>
2025-12-01 22:11:28 -0300

    Adiciona o Projeto Final de POO

---
+++ C#/ProjetoFinal/BlazorAppSistemaVendaBCC/BlazorAppSistemaVendaBCC/Entities/ItemVenda.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BlazorAppSistemaVendaBCC.Entities
+{
+    [Table("tbItemVenda")]
+    public class ItemVenda
+    {
+        [Column("ProdutoId")]
+        public int ProdutoId { get; set; }
+        public Produto Produto { get; set; }
+        [Column("PedidoId")]
+        public int PedidoId { get; set; }
+        public Pedido Pedido { get; set; }
+        public int QuantidadeProduto { get; set; }
+        public decimal PrecoUnitario { get; set; }
+    }
+}
